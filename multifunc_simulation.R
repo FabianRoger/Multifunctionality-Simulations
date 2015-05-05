4717a8b3ff572471ddded3bb6a791384e204c5e9
Fabian Roger <Fabian_Roger@gmx.de>
2015-05-05 15:51:43 +0200

    made null.model modular. nnumber of species, number of functions and distribution from which function values are drawn can be set

--- multifunc_simulation.R
+++ multifunc_simulation.R
@@ -21,156 +21,48 @@ library(gridExtra)
 # null model: substitutive and purely additive. multifunnc is average of weighted
 # average of single func (here: completely even so just arithmetic mean)
 
-null.model <- data.frame(SP = as.factor(rep(LETTERS[1:10],each=10)),
-                         Func = as.factor(rep(paste("F",formatC(c(1:10),width=2,flag="0"),sep="_"),10)),
-                         Value = as.numeric(round(runif(100,0,1),digits=2)))
-                         #Value = as.numeric(round(rnorm(100,2,1),digits=2)))
+#### define numper of Species (1 : 10000) ###
 
-# function to calculate mixture values of function at given richness
-avfunc <- function(R) {
-  species.sample <- sample(levels(null.model$SP),R,replace = FALSE)
-  sample.function.val <- null.model[null.model$SP %in%  species.sample,]
-  average.function <- ddply(sample.function.val, .(Func), summarise, av_Func = mean(Value))
-  return(average.function)
-  }
+spec.num <- 20
 
-# empty dataframe to store results from loop
-mixture.null <- data.frame(Func = character(), 
-                           av_Func = numeric(),
-                           Richness = numeric(),
-                           rep = numeric())
+### define number of Functions ##
 
-# populate dataframe for all richness levels from 1:10, with 100 replicates at each richness level
-for( R in 1:10) {
-  for( replicate in 1:100) {
-    temp.func <- avfunc(R)
-    temp.func$Richness <- R
-    temp.func$replicate <- replicate
-    mixture.null <- rbind(mixture.null,temp.func)
-  }
+func.num <- 15
+
+########## define distribution of function values ########## 
+
+## fixed sequence  ######## mean will depend on funcnum. have to fix that later
+Funcval_seq <- c()
+for (i in 1:10) {
+  Funcval_seq <-c(Funcval_seq, sample(seq(0.05,0.95,1/func.num),10,replace=F))
 }
 
-# cast dataframe in wide format to comply with the dataformat used in the multifunc package
-mixture_wide <- dcast( mixture.null, Richness + replicate ~ Func, value.var = "av_Func")
+## random from uniform
+Funcval_unif <- c()
+for (i in 1:10) {
+  Funcval_unif <-c(Funcval_unif, runif(func.num,0,1))
+}
 
-#################
-# In the following I replicate the analysis steps described 
-# In:
-# Supplementary Information 1: Using the multifunc package for analysis of 
-# Biodiversity Ecosystem Multifunctionality Relationships in R
-# from: 
-# Byrnes et al 2014; Investigating the relationship between biodiversity and 
-# ecosystem multifunctionality: challenges and solutions
+## random from normal with mean of 4 and sd 1
+Funcval_norm <- c()
+for (i in 1:10) {
+  Funcval_norm <-c(Funcval_norm, rnorm(func.num,4,1))
+}
 
-# The analysis above uses the german BIODEPTH data. 
-# For comarision, I replicate all performed here also with the original data
-# see Biodepth_example_JByrnes.R
+## random from gamma 
+Funcval_gamm <- c()
+for (i in 1:10) {
+  Funcval_gamm <-c(Funcval_gamm, rgamma(func.num,1))
+}
 
-# If I have to deviate from the analysis I specify it in the script
-###################
+## choose distribution
 
-############################
-# SINGLE FUNCTION APPROACH #
-############################
+#Funcval <- Funcval_seq
+#Funcval <- Funcval_unif
+#Funcval <- Funcval_norm
+Funcval <- Funcval_gamm
 
-# i dont calculate slopes and p-values as I'm primarily intrested in the graph here
 
-ggplot(mixture.null, aes(x = Richness, y = av_Func))+
-  geom_point(size=3)+
-  facet_wrap(~Func,nrow=2) +
-  theme_bw(base_size=15)+
-  stat_smooth(method="lm", colour="black", size=2) +
-  xlab("\nSpecies Richness") +
-  ylab("Value of Function\n") +
-  theme(panel.grid = element_blank())
+########## define null data set with parameters chosen above (Species, number functions, function values) #########
+
   
-
-#####################
-# OVERLAPP APPROACH #
-#####################
-
-#(I jump over this one for now)
-
-######################
-# AVERAGING APPROACH #
-######################
-
-#add on the new (standardized) functions along with the averaged multifunctional index
-mixture_wide <- cbind(mixture_wide, getStdAndMeanFunctions(mixture_wide, colnames(mixture_wide)[3:12]))
-
-#plot it
-ggplot(mixture_wide, aes(x=Richness, y=meanFunction))+
-    geom_point(size=3)+
-    theme_bw(base_size=15)+
-    stat_smooth(method="lm", colour="black", size=2) +
-    xlab("\nSpecies Richness") +
-    ylab("Average Value of Standardized Functions\n")
-
-#reshape for plotting everything with ggplot2 
-AvForPlot <- melt(mixture_wide[,c(1,13:23)], id.vars="Richness")
-
-#plot it 
-ggplot(AvForPlot, aes(x=Richness,y=value))+
-  geom_point(size=3)+ 
-  facet_wrap(~variable,nrow=2) +
-  theme_bw(base_size=15)+
-  stat_smooth(method="lm", colour="black", size=2) + 
-  xlab("\nSpecies Richness") +
-  ylab("Standardized Value of Function\n")
-
-######################
-# THRESHOLD APPROACH #
-######################
-
-mixedThresh <- getFuncsMaxed(mixture_wide, colnames(mixture_wide)[3:12], threshmin=0.05, threshmax=0.99, 
-                           prepend=c("Richness","replicate"), maxN=1)
-
-gcPlot_mixed <- subset(mixedThresh, mixedThresh$thresholds %in% qw(0.2, 0.4, 0.6, 0.8))
-gcPlot_mixed$percent <- paste(100*gcPlot_mixed$thresholds, "%", sep="")
-
-# note: Jerret uses GLMs to specify the best fit lines. As they don't converge in all cases for this dataframe,
-# I use just lm(). For the cases where the glm converged, the differences seem to be minimal.
-
-# plot 4 selected threshold values
-ggplot(gcPlot_mixed, aes(x=Richness, y=funcMaxed))+
-  geom_point()+
-  facet_wrap(~percent)+
-  stat_smooth(method="lm",colour="red", lwd=1.2 )+
-  labs(y = expression("Number of Functions" >= Threshold), x = ("Species Richness"))+
-  theme_bw(base_size=15)
-
-
-# plot N Function over Threshold ~ Richness Slopes for all Thresholds
-mixedThresh$percent <- 100*mixedThresh$thresholds 
-
-ggplot(data=mixedThresh, aes(x=Richness, y=funcMaxed, group=percent)) +
-  ylab(expression("Number of Functions" >= Threshold)) +
-  xlab("Species Richness") +
-  stat_smooth(method="lm", lwd=0.8, fill=NA, aes(color=percent))+
-  theme_bw(base_size=14) +
-  scale_color_gradient(name="Percent of \nMaximum", low="blue", high="red")
-
-# plot the slopes of the relationship against Threshold values
-
-# note that I take fun = lm as glm doesn't converge for all thresholds over 67%
-# however, the parts that converge look almost identical so it's no big deal
-
-mixedLinearSlopes<-getCoefTab(funcMaxed ~ Richness, fun = lm,  data=mixedThresh, coefVar="Richness")
-
-ggplot(mixedLinearSlopes, aes(x=thresholds)) +
-  geom_ribbon(fill="grey50", aes(x=thresholds*100, ymin=Estimate- 1.96*germanyLinearSlopes[["Std. Error"]],
-                                 ymax=Estimate+1.96*germanyLinearSlopes[["Std. Error"]])) + 
-  geom_point(aes(x=thresholds*100, y=Estimate)) +
-  ylab("Change in Number of Functions per Addition of 1 Species\n") + xlab("\nThreshold (%)") +
-  stat_abline(intercept=0, slope=0, lwd=1, linetype=2) + 
-  theme_bw(base_size=14)
-
-# highlight critical slopes
-
-# We can't calculate Tmin, Tmax and Tmde because the calculation rely on the fact that the slopes get non-significant at
-# some point. Becasue these are simulated data with 100 replicates for each richness level, this never happens (all slopes
-# are significantly differnt from 0 at all threshold levels) . 
-
-
-
-
