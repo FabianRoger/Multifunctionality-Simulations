8d60729fedf07ef23a037cc087745a10bf452512
Fabian Roger <fabian.roger@bioenv.gu.se>
2017-03-21 16:40:42 +0100

    added mvrnorm function from the MASS package to avoid having to laod it as it conflicts with dplyr

--- Multifunctionality_simulations/Multifunc_simulations_functions.R
+++ Multifunctionality_simulations/Multifunc_simulations_functions.R
@@ -490,3 +490,43 @@ getCoefTab<-function(eqn, fun=glm, data, groupVar="thresholds", coefVar, ...){
 ############################################
 
 
+
+########### function taken form the MASS package ###################
+
+# The function below is taken from the MASS package 
+
+# Venables, W. N. & Ripley, B. D. (2002) Modern Applied Statistics with S. Fourth Edition. Springer, New York. ISBN
+# 0-387-95457-0
+
+# It is included here in it's original form (I just copy pasted it here) as
+# the package conflicts with dplyr if loaded afterwards (it overwrites dplyr::select)
+
+mvrnorm <- function (n = 1, mu, Sigma, tol = 1e-06, empirical = FALSE, EISPACK = FALSE) 
+{
+  p <- length(mu)
+  if (!all(dim(Sigma) == c(p, p))) 
+    stop("incompatible arguments")
+  if (EISPACK) 
+    stop("'EISPACK' is no longer supported by R", domain = NA)
+  eS <- eigen(Sigma, symmetric = TRUE)
+  ev <- eS$values
+  if (!all(ev >= -tol * abs(ev[1L]))) 
+    stop("'Sigma' is not positive definite")
+  X <- matrix(rnorm(p * n), n)
+  if (empirical) {
+    X <- scale(X, TRUE, FALSE)
+    X <- X %*% svd(X, nu = 0)$v
+    X <- scale(X, FALSE, TRUE)
+  }
+  X <- drop(mu) + eS$vectors %*% diag(sqrt(pmax(ev, 0)), p) %*% 
+    t(X)
+  nm <- names(mu)
+  if (is.null(nm) && !is.null(dn <- dimnames(Sigma))) 
+    nm <- dn[[1L]]
+  dimnames(X) <- list(nm, NULL)
+  if (n == 1) 
+    drop(X)
+  else t(X)
+}
+
+
